6ef88e3ce0bbe64cd2539bebeef434b47be9932d
Yelkira <penda.fu@gmail.com>
2023-03-09 17:42:36 +0300

    hw 10 is done

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,11 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () =>
+                <div className={s.spinner}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,17 @@
-const initState = {
+export type InitStateType = {
+    isLoading: boolean
+}
+
+const initState:InitStateType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":{
+            return {...state, isLoading:action.isLoading}
+        }
         default:
             return state
     }
